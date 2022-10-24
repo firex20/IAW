8a8e8e5b92d43b7908713d6371175594c798f3fd
Pedro <molden14@gmail.com>
2022-10-24 13:53:15 +0200

    Practica menu y bootstrap añadido

---
+++ menu/src/App.js
@@ -0,0 +1,12 @@
+import './bootstrap/dist/css/bootstrap.min.css';
+import Menu from './Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
